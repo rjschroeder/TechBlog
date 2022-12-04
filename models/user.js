9227adebb201b6176f5107f2193e80145157b81a
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 15:48:36 -0600

    id property of user

--- models/user.js
+++ models/user.js
@@ -9,4 +9,15 @@ class User extends Model {
     }
 }
 
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        }
+    }
+)
+
 module.exports = User;

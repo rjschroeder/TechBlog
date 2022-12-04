5642749c5063e6592dade654fb508731d10b085c
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 15:46:04 -0600

    checkpassword function for user model
    uses bcrypt and sequelize model

--- models/user.js
+++ models/user.js
@@ -2,3 +2,11 @@ const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 //no sequelize connection yet
+
+class User extends Model {
+    checkPassword(loginPassword) {
+        return bcrypt.compareSync(loginPassword, this.password);
+    }
+}
+
+module.exports = User;

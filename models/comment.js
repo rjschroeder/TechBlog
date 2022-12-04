b9116dc39ba4b29b00b03d39c8b13094bc17a777
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 16:17:48 -0600

    comment model
    pasted from post model, was considering consolidating into one model but decided for orm reasons it was better to have 2 separate models

---
+++ models/comment.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+//no sqlize connection yet
+
+class Comment extends Model {};
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "comment"
+    }
+)
+
+module.exports = Comment;

6bce20854ec12d5ec2745b2b69b4dc8202066178
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 16:12:42 -0600

    post model with sequelize model paramaters

--- models/post.js
+++ models/post.js
@@ -2,3 +2,28 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 //no sqlize connection yet
 
+class Post extends Model {};
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "post"
+    }
+)
+
+module.exports = Post;

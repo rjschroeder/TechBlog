6939eb3f52245b360d76cb6ad201b895e0529d53
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 23:37:24 -0600

    query selectors for inputs

---
+++ public/js/login.js
@@ -0,0 +1,9 @@
+let loginForm = document.querySelector("#loginForm");
+
+let login = async function(event) {
+    event.preventDefault();
+    let enteredUsername = document.querySelector("#username-input");
+    let enteredPassword = document.querySelector("#password-input");
+
+    
+}

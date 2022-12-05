606c53be4c8a71aea1fa97c1856e5685332bc56f
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 19:57:18 -0600

    returns comment for new comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -6,7 +6,7 @@ router.post("/new", withAuth, async (req, res) => {
     try {
         let reqbody = req.body;
         let newComment = await Comment.create({...reqbody, user_id: req.session.user_id})
-        res.status(200).json(newPost);
+        res.status(200).json(newComment);
     } catch (err) {
         res.status(500).json(err);
     }

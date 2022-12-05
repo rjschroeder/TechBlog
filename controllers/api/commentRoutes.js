500904467776a4c0a4a008438f11b8c8e54cb6df
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 21:36:05 -0600

    temporary fix on new comment
    attaching which post id in the req.params until I can get the front end done

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,10 +2,13 @@ const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-router.post("/new", withAuth, async (req, res) => {
+router.post("/new/:postid", withAuth, async (req, res) => {
     try {
         let reqbody = req.body;
-        let newComment = await Comment.create({...reqbody, user_id: req.session.user_id})
+        let newComment = await Comment.create({
+            ...reqbody, 
+            post_id: req.params.postid,
+            user_id: req.session.user_id})
         res.status(200).json(newComment);
     } catch (err) {
         res.status(500).json(err);

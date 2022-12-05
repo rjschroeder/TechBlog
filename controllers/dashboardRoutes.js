03155fa760d9cb0885b8458cdca0cadcd712dc24
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 21:41:37 -0600

    initialized dashboard routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,3 +1,29 @@
 const router = require("express").Router();
 const {Comment, Post, User} = require("../models");
 const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        //redirect 
+    }
+})
+
+router.get("/new", withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        //redirect 
+    }
+})
+
+router.get("/edit/:id", withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        //redirect 
+    }
+})
+
+module.exports = router;

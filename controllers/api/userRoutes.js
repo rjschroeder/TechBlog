5d937f99c6f7cc478c316abc60d8a5bd8d749d0e
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 19:11:26 -0600

    initialized all routes for user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,1 +1,28 @@
 const router = require("express").Router();
+const { User } = require("../../models");
+
+router.post("/new", async (req, res) => {
+    try {
+
+    } catch (err) {
+        req.status(500).json(err);
+    }
+})
+
+router.post("/login", async (req, res) => {
+    try {
+
+    } catch (err) {
+        req.status(500).json(err);
+    }
+})
+
+router.post("/logout", async (req, res) => {
+    try {
+
+    } catch (err) {
+        req.status(500).json(err);
+    }
+})
+
+module.exports = router;

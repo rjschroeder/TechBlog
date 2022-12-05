c8f92e841097dab4367c1de62b76f6b9cdb32aa1
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 19:54:37 -0600

    create new comment route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,6 +2,14 @@ const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-
+router.post("/new", withAuth, async (req, res) => {
+    try {
+        let reqbody = req.body;
+        let newComment = await Comment.create({...reqbody, user_id: req.session.userId})
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -5,7 +5,7 @@ const withAuth = require("../../utils/auth");
 router.post("/new", withAuth, async (req, res) => {
     try {
         let reqbody = req.body;
-        let newPost = await Post.create({...reqbody, userId: req.session.userId})
+        let newPost = await Post.create({...reqbody, user_id: req.session.userId})
         res.status(200).json(newPost);
     } catch (err) {
         res.status(500).json(err);

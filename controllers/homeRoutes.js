8874f379c762bc970a0d92b73bb81a3016f1c616
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 20:05:03 -0600

    initialized get function in home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,36 @@ const router = require("express").Router();
 const {Comment, Post, User} = require("../models");
 const withAuth = require("../utils/auth");
 
+router.get("/", withAuth, async (req, res) => {
+    try {
 
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get("/post/:id", withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get("/login", withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get("/signip", withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;

831c59f5477299bf5a2ec7983bf58bc7822aeeba
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 19:56:47 -0600

    refactored session variables to match

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -5,7 +5,7 @@ const withAuth = require("../../utils/auth");
 router.post("/new", withAuth, async (req, res) => {
     try {
         let reqbody = req.body;
-        let newComment = await Comment.create({...reqbody, user_id: req.session.userId})
+        let newComment = await Comment.create({...reqbody, user_id: req.session.user_id})
         res.status(200).json(newPost);
     } catch (err) {
         res.status(500).json(err);

20ca0b369f396dffa5f3204ad0c2e75f71dcc65c
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 23:39:50 -0600

    api call for login

--- public/js/login.js
+++ public/js/login.js
@@ -5,5 +5,12 @@ let login = async function(event) {
     let enteredUsername = document.querySelector("#username-input");
     let enteredPassword = document.querySelector("#password-input");
 
-    
+    let response = await fetch("/api/users/login", {
+        method: "POST",
+        body: JSON.stringify({
+            username: enteredUsername.value,
+            password: enteredPassword.value
+        })
+    })
+
 }

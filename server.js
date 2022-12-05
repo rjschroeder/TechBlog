12ddbd11727f13f488cf9b8e79a467083b2b78f1
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 18:24:53 -0600

    set up new express session
    includes secret, cookie for login

--- server.js
+++ server.js
@@ -9,3 +9,22 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = expressHandlebars.create({ helpers });
+
+const sess = {
+    secret: "I aint neva told no one this",
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: "Strict"
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+
+app.use(session(sess));

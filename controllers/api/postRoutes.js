54d53fa5d0c469efa37701648acca7e6d52d90d9
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 19:32:45 -0600

    wrote initial routes for post, put, and delete

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,29 @@
 const router = require("express").Router();
 const { Post } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+router.post("/new", withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.put("/:id", withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.delete("/:id", withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;

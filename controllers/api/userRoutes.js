c04d35ca539f289274f045b0e1c8f3f237777f77
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 19:15:40 -0600

    create a new user at /new route with req.body
    also update session variables

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,7 +3,15 @@ const { User } = require("../../models");
 
 router.post("/new", async (req, res) => {
     try {
+        const userData = await User.create(req.body);
 
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        })
     } catch (err) {
         req.status(500).json(err);
     }

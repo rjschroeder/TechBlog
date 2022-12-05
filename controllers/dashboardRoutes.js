6a232f93298092241937017d36c2473b0db41d5a
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-05 01:22:10 -0600

    updated route for getting all posts

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -9,9 +9,13 @@ router.get("/", withAuth, async (req, res) => {
                 user_id: req.session.user_id
             }
         })
-        //render posts
+        let posts = postData.map((post) => post.get({plain:true}));
+        res.render('feed', {
+            layout: "dashboard",
+            posts
+        })
     } catch (err) {
-        //redirect 
+        res.redirect("login")
     }
 })
 

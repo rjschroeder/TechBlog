597cfc27cafacba8183752225460bed2590fb420
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-05 00:26:59 -0600

    signup and login handlebars
    had accidentally created login with <small>s and email input, decided to paste it into sign up and remove them to make the login. Also refactored login to sign up.

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,12 +3,14 @@ let signupForm = document.querySelector("#signupForm");
 let signup = async function(event) {
     event.preventDefault();
     let enteredUsername = document.querySelector("#username-input");
+    let enteredEmail = document.querySelector("#email-input");
     let enteredPassword = document.querySelector("#password-input");
 
     let response = await fetch("/api/users/new", {
         method: "POST",
         body: JSON.stringify({
             username: enteredUsername.value,
+            email: enteredEmail.value,
             password: enteredPassword.value
         })
     })

195324918f09568ee599def309b27bcd50c2cbd4
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-04 23:48:34 -0600

    api call for new post

--- public/js/new.js
+++ public/js/new.js
@@ -5,6 +5,19 @@ let newPost = async function(event) {
     let enteredTitle = document.querySelector("#title-input");
     let enteredContent = document.querySelector("#content-input");
 
+    let response = await fetch("/api/posts/new", {
+        method: "POST",
+        body: JSON.stringify({
+            title: enteredTitle.value,
+            content: enteredContent.value
+        })
+    })
+
+    if(response.ok) {
+        document.location.replace("/dashboard");
+    } else {
+        console.log("new post failed");
+    }
 }
 
 newPostForm.addEventListener("submit", newPost);
